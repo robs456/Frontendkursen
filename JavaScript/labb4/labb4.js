18b0ff8756cde8c6e95b69b78e4f0351a58ce5aa
robs456 <robsve73@gmail.com>
2022-09-19 12:32:31 +0200

    Labb4 finished

---
+++ JavaScript/labb4/labb4.js
@@ -0,0 +1,24 @@
+let num1 = 10;
+let num2 = 2;
+
+
+
+console.log("Nummer 1 är: " + num1);
+console.log("Nummer 2 är: " + num2);
+console.log("Nummer 1 + Nummer 2 är: " + (num1+num2));
+console.log("Nummer 1 - Nummer 2 är: " + (num1-num2));
+console.log("Nummer 1 * Nummer 2 är: " + (num1*num2));
+console.log(num1 + " plus " + num2 + " är: " + (num1+num2));
+
+const person = {
+    Förnamn : "Robert",
+    Efternamn : "Svensson",
+    Ålder : 49
+
+ }
+
+ console.log("Hej, mitt namn är " + person.Förnamn + " "
+  + person.Efternamn + " och jag är " + 
+  person.Ålder + " år gammal");
+
+  
