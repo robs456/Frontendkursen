abd5300cf739c0aeb04448984236c352a33960b7
robs456 <robsve73@gmail.com>
2022-12-12 12:32:23 +0100

    QP

--- Vue/Labb20/AMCM/src/views/products/ProductDetails.vue
+++ Vue/Labb20/AMCM/src/views/products/ProductDetails.vue
@@ -7,7 +7,7 @@
         <div class="right">
             <h2>{{productStore.deetProd.name}}</h2>
             <p class="longDesc">{{ productStore.deetProd.longDesc }}</p>
-            <p class="button " @click="handleClick()">Details</p>
+            <p class="button button2" @click="handleClick()">Details</p>
         </div>
        
         
@@ -27,22 +27,68 @@ import { useProductStore } from '../../stores/ProductStore'
 <style >
 .container{
      display: flex;
+     justify-content: space-evenly;
+
 }
 
 .left{
-    width: 45;
+    width: 45vw ;
+    margin: 2em 0.625em; 
+    display: flex;
+    flex-direction: column;
+    
 }
 
 .right{
-  
+    width: 45vw;
+    margin: 2em auto;
+    padding: 3em;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+
 }
 
 .productImage2{
-  margin: 0 auto; 
-  width: 300px;
-  height: 500px;
+    margin: 0 auto;
+    width: 40vw;
+    
+    border-radius: 3rem;
+}
+
+h2 {
+    margin-bottom: .625em;
 }
 .longDesc{
-   
+   margin-bottom: 1em;
+   max-width: 40vw;
+}
+.button2{
+    text-align: left;
+    justify-self: left;
+}
+
+@media screen and (max-width:600px){
+
+.container{
+    flex-direction: column;  
+    justify-content: center;
+}
+.productImage2{
+    margin: 0 ;
+    width: 40vw;
+    
+    border-radius: 1rem;
+}
+
+.right{
+    width: 100vw;
+    
+}
+.left{
+    width: 100vw;
+    
+}
+
 }
 </style>
