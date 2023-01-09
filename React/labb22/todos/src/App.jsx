c6dda5b6ab18d02601f8547b81386bbb61573acc
robs456 <robsve73@gmail.com>
2023-01-09 17:00:11 +0100

    Labb 22 upgrade w localstorage

--- React/labb22/todos/src/App.jsx
+++ React/labb22/todos/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Header from './Components/Header'
 import TodoList from './Components/TodoList'
-import TodoItem from './Components/TodoItem'
 import { EditModal } from './Components/EditModal'
 import { NukeModal } from './Components/NukeModal'
 import { AddModal } from './Components/AddModal'
 
 
 function App() {
-  const [todos, setTodos] = useState([
-    {title: 'Wag the dog', completed : false, id: 1},
-    {title: 'trash', completed : true, id: 2},
-    {title: 'dishes', completed : false, id: 3},
-    {title: 'yo mama', completed : false, id: 4}
-  ])
+
+  // For troubleshooting or demo:
+  // const [todos, setTodos] = useState([
+  //   {title: 'Wag the dog', completed : false, id: 1},
+  //   {title: 'trash', completed : true, id: 2},
+  //   {title: 'dishes', completed : false, id: 3},
+  //   {title: 'yo mama', completed : false, id: 4}
+  // ])
+
+  const [todos, setTodos] = useState([])
 
   const [showEditModal, setShowEditModal] = useState(false)
   const [showAddModal, setShowAddModal] = useState(false)
@@ -33,9 +36,7 @@ function App() {
     setShowNukeModal(false)
   }
 
-  const addItem = (item) => {
-    console.log(item)
-  
+  const addItem = (item) => {  
     setTodos(prevState => {
       return [...prevState, { id: crypto.randomUUID(), completed: false, title: item }]
     })
@@ -65,6 +66,20 @@ function App() {
     setTodos(prevState => {
       return prevState.filter(todo => todo.id !== id)})
   } 
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todoList'))
+    if(storedTodos && storedTodos.length) {
+      setTodos(storedTodos)
+    }
+  }, [])
+
+  
+  useEffect(() => {
+    
+      localStorage.setItem('todoList', JSON.stringify(todos))
+   
+  }, [todos])
 
   return (
     <div className="App">
